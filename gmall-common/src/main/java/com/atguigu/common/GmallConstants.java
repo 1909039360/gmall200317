3c3f3cee4de57429be94d219172d5e13df810171
doubleZ <1909039360@qq.com>
2020-08-19 20:17:15 +0800

    2020年8月19日20:15:52

--- gmall-common/src/main/java/com/atguigu/common/GmallConstants.java
+++ gmall-common/src/main/java/com/atguigu/common/GmallConstants.java
@@ -33,6 +33,27 @@ public class GmallConstants {
     public static final String GMALL_TOPIC_ORDER_DETAIL = "TOPIC_ORDER_DETAIL";
     //用户信息主题
     public static final String GMALL_TOPIC_USER_INFO = "TOPIC_USER_INFO";
+    public static final String GMALL_TOPIC_USER_INFO = "TOPIC_USER_INFO";
+    public static final String GMALL_TOPIC_USER_INFO = "TOPIC_USER_INFO";
+    public static final String GMALL_TOPIC_USER_INFO = "TOPIC_USER_INFO";
+    public static final String GMALL_TOPIC_USER_INFO = "TOPIC_USER_INFO";
+    public static final String GMALL_TOPIC_USER_INFO = "TOPIC_USER_INFO";
+    public static final String GMALL_TOPIC_USER_INFO = "TOPIC_USER_INFO";
+    public static final String GMALL_TOPIC_USER_INFO = "TOPIC_USER_INFO";
+    public static final String GMALL_TOPIC_USER_INFO = "TOPIC_USER_INFO";
+    public static final String GMALL_TOPIC_USER_INFO = "TOPIC_USER_INFO";
+    public static final String GMALL_TOPIC_USER_INFO = "TOPIC_USER_INFO";
+    public static final String GMALL_TOPIC_USER_INFO = "TOPIC_USER_INFO";
+    public static final String GMALL_TOPIC_USER_INFO = "TOPIC_USER_INFO";
+    public static final String GMALL_TOPIC_USER_INFO = "TOPIC_USER_INFO";
+    public static final String GMALL_TOPIC_USER_INFO = "TOPIC_USER_INFO";
+    public static final String GMALL_TOPIC_USER_INFO = "TOPIC_USER_INFO";
+    public static final String GMALL_TOPIC_USER_INFO = "TOPIC_USER_INFO";
+    public static final String GMALL_TOPIC_USER_INFO = "TOPIC_USER_INFO";
+    public static final String GMALL_TOPIC_USER_INFO = "TOPIC_USER_INFO";
+    public static final String GMALL_TOPIC_USER_INFO = "TOPIC_USER_INFO";
+    public static final String GMALL_TOPIC_USER_INFO = "TOPIC_USER_INFO";
+
 
 }
 
