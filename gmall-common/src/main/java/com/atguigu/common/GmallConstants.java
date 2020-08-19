ade6ddd0a32e07af18180fc072d222daae40d9ba
doubleZ <1909039360@qq.com>
2020-08-19 20:11:25 +0800

    2020年8月19日20:07:30

--- gmall-common/src/main/java/com/atguigu/common/GmallConstants.java
+++ gmall-common/src/main/java/com/atguigu/common/GmallConstants.java
@@ -24,7 +24,7 @@ public class GmallConstants {
     public static final String ES_INDEX_SALE_DETAIL="gmall2020_sale_detail";
     //ES中预警日志索引前缀
     public static final String GMALL_ES_ALERT_INFO_PRE = "gmall_coupon_alert";
-
+//sdfsfsfsfsfsf
     //事件日志主题
     public static final String GMALL_TOPIC_EVENT = "TOPIC_EVENT";
 
